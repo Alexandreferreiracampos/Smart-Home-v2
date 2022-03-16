e7531367f542bbce42ae745831ae938c5fe0afbf
Alexandre Campos <alexandrerazrmaxx@gmail.com>
2022-03-16 09:37:09 -0300

    Adionado Splash

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image, ToastAndroid, Modal, ScrollView} from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image, ToastAndroid, Modal, ScrollView } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
 import { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -11,7 +11,6 @@ import BTBedroom from '../assets/Bedroom.png';
 import BTLivingRoom from '../assets/sofa.png';
 import escritorio from '../assets/escritorio.png';
 import churrasco from '../assets/churrasco.png';
-import lamp from '../assets/lamp.png'
 import * as LocalAuthentication from 'expo-local-authentication';
 
 
@@ -26,7 +25,7 @@ export default function Home() {
     const navigation = useNavigation();
 
 
-    const [devices, setDevices] = useState({ fan:'',Bedroom:'',livingRoom:'', name:'',escritorio:'', edicula:'' });
+    const [devices, setDevices] = useState({ fan: '', Bedroom: '', livingRoom: '', name: '', escritorio: '', edicula: '' });
 
     if (validateData == true) {
         async function loadStorgeUserName() {
@@ -74,13 +73,13 @@ export default function Home() {
         req.send()
 
         switch (valor) {
-            case devices.livingRoom+"/rele1":
+            case devices.livingRoom + "/rele1":
                 setActiveTextLeds(!activeTextLeds)
                 break;
-            case devices.livingRoom+"/?rele4":
+            case devices.livingRoom + "/?rele4":
                 setActiveTextArandela(!activeTextArandela)
-                    break;
-            case devices.livingRoom+"?rele3":
+                break;
+            case devices.livingRoom + "?rele3":
                 setActiveTextGaragem(!activeTextGaragem)
                 break;
 
@@ -89,27 +88,25 @@ export default function Home() {
 
     }
 
-    const biometric = async ()=>{
+    const biometric = async () => {
 
-        
+
         const authenticationBiometric = await LocalAuthentication.authenticateAsync({
-          promptMessage:"Portão eletrônico",
-          cancelLabel: "Cancelar",
-          disableDeviceFallback: false,
+            promptMessage: "Portão eletrônico",
+            cancelLabel: "Cancelar",
+            disableDeviceFallback: false,
         });
-      
-        if(authenticationBiometric.success){
-          openGate() 
+
+        if (authenticationBiometric.success) {
+            openGate()
         }
 
+    };
 
-        
-      };
-      
 
     const openGate = () => {
 
-        command(devices.livingRoom+"/relea")
+        command(devices.livingRoom + "/relea")
 
         ToastAndroid.showWithGravityAndOffset(
             "Acionando Portão",
@@ -117,7 +114,7 @@ export default function Home() {
             ToastAndroid.CENTER,
             25,
             50
-          );
+        );
 
     }
 
@@ -125,9 +122,9 @@ export default function Home() {
     return (
 
         <View style={styles.container}>
-            
+
             <ScreenModal statusModal={modalActive} ipFan={devices.fan} ipBedroom={devices.Bedroom} ipLivingRoom={devices.livingRoom} name={devices.name} ipEdicula={devices.edicula} ipEscritorio={devices.escritorio} changeStatusModal={() => changeStatusModal()} reloadDataSave={() => reloadDataSave()} />
-           
+
             <View style={styles.header}>
                 <Animatable.Text animation="slideInLeft" style={styles.title}>Olá {devices.name}</Animatable.Text>
                 <Animatable.Text animation="slideInRight" onPress={() => setModalAtive(true)}><FontAwesome name="gears" size={24} color='#868686' /></Animatable.Text>
@@ -137,26 +134,26 @@ export default function Home() {
                 <Image source={require('../assets/home.jpeg')} style={styles.image}></Image>
             </View>
 
-            <View style={{top:-28, width: "100%", height: "9%", backgroundColor: 'white', justifyContent: 'center', alignItems: 'center',flexDirection: 'row', borderTopRightRadius:20, borderBottomRightRadius:20 }}>
+            <View style={{ top: -28, width: "100%", height: "9%", backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', borderTopRightRadius: 20, borderBottomRightRadius: 20 }}>
                 <View style={{ width: 130, height: 130, borderRadius: 75, backgroundColor: 'white', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                <TouchableOpacity onPress={() => biometric()}>
-                    <View style={styles.ButtonGate}>
-                    <Image source={Gate} style={{ width: '60%', height: '60%' }} />
-                    </View>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => biometric()}>
+                        <View style={styles.ButtonGate}>
+                            <Image source={Gate} style={{ width: '60%', height: '60%' }} />
+                        </View>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity style={styles.button} onPress={()=>command(devices.livingRoom+"/rele1")} ><Text style={[styles.titleButton, activeTextLeds && styles.titleButtonActive]}>Leds</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={()=>command(devices.livingRoom+"/?rele4")}><Text style={[styles.titleButton, activeTextArandela && styles.titleButtonActive]}>Arandelas</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={()=>command(devices.livingRoom+"?rele3")}><Text style={[styles.titleButton, activeTextGaragem && styles.titleButtonActive]}>Garagem</Text></TouchableOpacity>
-             </View>
+                <TouchableOpacity style={styles.button} onPress={() => command(devices.livingRoom + "/rele1")} ><Text style={[styles.titleButton, activeTextLeds && styles.titleButtonActive]}>Leds</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => command(devices.livingRoom + "/?rele4")}><Text style={[styles.titleButton, activeTextArandela && styles.titleButtonActive]}>Arandelas</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => command(devices.livingRoom + "?rele3")}><Text style={[styles.titleButton, activeTextGaragem && styles.titleButtonActive]}>Garagem</Text></TouchableOpacity>
+            </View>
 
             <View style={styles.containerButton} >
                 <ScrollView horizontal contentContainerStyle={styles.environmentList} showsHorizontalScrollIndicator={false} >
-                <Button title='Sala' ico={BTLivingRoom} width={90} height={90} onPress={() => navigatioScreen('LivingRoom')} />
-                <Button title='Quarto' ico={BTBedroom} width={90} height={90} onPress={() => navigatioScreen('Bedroom')} />
-                <Button title='Edícula' ico={churrasco} width={90} height={90} onPress={()=>navigatioScreen('PartyArea')}/>
-                <Button title='Escritório' ico={escritorio} width={90} height={90} onPress={() => navigatioScreen('GamerRoom')} />
-                
+                    <Button title='Sala' ico={BTLivingRoom} width={90} height={90} onPress={() => navigatioScreen('LivingRoom')} />
+                    <Button title='Quarto' ico={BTBedroom} width={90} height={90} onPress={() => navigatioScreen('Bedroom')} />
+                    <Button title='Edícula' ico={churrasco} width={90} height={90} onPress={() => navigatioScreen('PartyArea')} />
+                    <Button title='Escritório' ico={escritorio} width={90} height={90} onPress={() => navigatioScreen('GamerRoom')} />
+
                 </ScrollView>
             </View>
 
@@ -213,7 +210,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgb(243,243,243)',
         justifyContent: 'center',
         alignItems: 'center',
-        
+
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -224,14 +221,14 @@ const styles = StyleSheet.create({
         elevation: 9,
 
     },
-    button:{
-        width:"19%",
-        height:"56%",
-        margin:5,
-        backgroundColor:'rgb(243,243,243)',
-        borderRadius:10,
-        justifyContent:'center',
-        alignItems:'center',
+    button: {
+        width: "19%",
+        height: "56%",
+        margin: 5,
+        backgroundColor: 'rgb(243,243,243)',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -252,12 +249,12 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgb(243,243,243)'
 
     },
-    environmentList:{
-        width:'190%',
-        height:"100%",
-        justifyContent:'center',
-        alignItems:'center',
-        paddingHorizontal:"42%"
+    environmentList: {
+        width: '190%',
+        height: "100%",
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: "42%"
     }
 
 })
